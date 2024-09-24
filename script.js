ea4af0973bd1ecdacb08b017c440a55bdc0e56c4
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-09-24 12:12:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" || answer == "pealed") alert("Correct!");
-  else alert("Wrong, Batman! Watch the video, and try again.");
+var answer = prompt("What gets bigger when you take more out of it?");
+if (answer == "A hole" | "a hole") alert("yes, BATMAN.");
+else alert("incorrect.  BATMAN.");
 }
